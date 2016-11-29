b79a13d55ee83f93e197a9b87bb72a82036d274e
shj <359137962@qq.com>
2016-11-29 17:15:11 +0800

    houselist

---
+++ Tornado_Project_Day06_v1.1/Tornado_Project/urls.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+
+import os
+
+from handlers import Passport, VerifyCode, Profile, House
+from handlers.BaseHandler import StaticFileHandler
+
+handlers = [
+    (r"/api/imagecode", VerifyCode.ImageCodeHandler),
+    (r"/api/smscode", VerifyCode.SMSCodeHandler),
+    (r'^/api/register$', Passport.RegisterHandler),
+    (r'^/api/login$', Passport.LoginHandler),
+    (r'^/api/check_login$', Passport.CheckLoginHandler),
+    (r'^/api/logout$', Passport.LogoutHandler),
+    (r'^/api/profile$', Profile.ProfileHandler),
+    (r'^/api/profile/avatar$', Profile.AvatarHandler),
+    (r'^/api/profile/name$', Profile.NameHandler),
+    (r'^/api/profile/auth$', Profile.AuthHandler),
+    (r'^/api/house/info$', House.HouseInfoHandler),
+    (r'^/api/house/image$', House.HouseImageHandler),
+    (r'^/api/house/area$', House.AreaInfoHandler),
+    (r'^/api/house/my$', House.MyHousesHandler),
+    (r'^/api/house/index$', House.IndexHandler),
+    (r'^/api/house/list$', House.HouseListHandler),
+    (r'^/api/house/listv2$', House.HouseListHandlerV2),
+    (r"/(.*)", StaticFileHandler, dict(path=os.path.join(os.path.dirname(__file__), "html"), default_filename="index.html"))
+]
